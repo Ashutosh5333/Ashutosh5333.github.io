61068eba57deff0247d609824380fcb787db826f
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-02-27 09:50:54 +0530

    skill updated

---
+++ src/Components/Skill.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Skill = () => {
+  return (
+    <div>
+    Skill
+    
+    </div>
+  )
+}
+
+export default Skill
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import "remixicon/fonts/remixicon.css"
-import reportWebVitals from './reportWebVitals';
 import "./index.css"
-import {ChakraProvider} from "@chakra-ui/react"
+
+
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
  
+
     <App />
+  
  
   </React.StrictMode>
 );
