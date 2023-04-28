e3fda3530751d07b4f1066a873c5644d5cfe9846
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-04-28 19:00:16 +0530

    added css

--- src/Components/PersonalAchive/PersonalAchive.jsx
+++ src/Components/PersonalAchive/PersonalAchive.jsx
@@ -1,9 +1,8 @@
-import { Box, Image, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box,  Image,  Text } from '@chakra-ui/react'
 import React from 'react'
-import STAR from "./STAR.jpeg" 
-import Project from "./Project.jpeg"
-import codingimg from "./codingimg.jpg"
-import miniproject from "./miniproject.jpeg"
+import "./personal.css"
+
+import {Data} from  "./Data"
 
 
 const PersonalAchive = () => {
@@ -11,33 +10,23 @@ const PersonalAchive = () => {
 
   return (
     <>
-     <Box border={"2px solid red"} >
-         <Text textAlign={"center"} fontSize={"3rem"} fontWeight={"500"}> Personal Achivement </Text>
-         {/* columns={[1, 3, 4]}  */}
-         {/* columns={{sm: 2, md: 3}} */}
-         {/* <Box   border={"3px solid green"}  height="50vh" width="90vw" m="auto" > */}
-               <SimpleGrid  minChildWidth='120px' spacing='40px' gap={5}>
-
-              <Box>
-                  <Image src={STAR} width="300px" height={"300px"}/>
-              </Box>
-              
-              <Box>
-                  <Image src={codingimg}  width="300px" height={"300px"} />
-              </Box>
-              
-              <Box>
-                  <Image  src={miniproject}  width="300px" height={"300px"} />
-              </Box>
-
-               <Box>
-                  <Image src={Project}  width="300px" height={"300px"} />
-              </Box>
-
-               </SimpleGrid>
+     <Box className='personalcontainer'>
+         <Text  textAlign={"center"} fontSize={"3rem"} fontWeight={"500"} color="#FFFFFF"> Personal Achivement </Text>
+        
+         <Box className="personalAchive"> 
+             
+             {
+              Data.length>0 && Data.map((el) =>{
+                return <Box  m="auto" className='Card' >
+                <Image className='img' src={el.img} 
+                   width="300px"
+                  height="350px" 
+                 />                  
+                </Box>
+              })
+             }
            
-          {/* </Box> */}
-
+               </Box>
 
      </Box>
     </>
